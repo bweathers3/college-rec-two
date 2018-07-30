8701c27820740f9f7fd17c0c24554f19085930c9
Bailey <bailey.g.weathers@gmail.com>
2018-07-29 20:40:09 -0500

    npm install react-router-dom plus basic app page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,7 +5,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <header className="App-header">
+          <h1 className="App-title">Welcome to GetRecruited.com </h1>
+        </header>
+        <p className="App-intro">
+          What to be a college athlete? Get started today. Make your dream come true.
+        </p>
+        <p className="App-footer">
+          <p>Footer</p>
+        </p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/app/App';
+//import Home from './pages/Home';
 import registerServiceWorker from './registerServiceWorker';
-
 import { Router, Route } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*
+ const router = (
+     <BrowserRouter>
+       <Route path="/" component={ App }>
+         <App />
+       </Route>
+     </BrowserRouter>
+ )
+
+
+ ReactDOM.render(
+   router,
+   document.getElementById('root')
+ );
+ */
+
+ ReactDOM.render(
+     <BrowserRouter>
+       <App />
+     </BrowserRouter>
+     , document.getElementById('root'));
+
 registerServiceWorker();
