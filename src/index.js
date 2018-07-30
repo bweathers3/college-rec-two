0f67296d3992ba946e31bc840fef73a524d1ad02
Bailey <bailey.g.weathers@gmail.com>
2018-07-29 19:04:45 -0500

    complete new file structure for app components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/app/App';
 import registerServiceWorker from './registerServiceWorker';
+
+import { Router, Route } from 'react-router';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
